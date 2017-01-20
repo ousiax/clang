e17dd5aa09e06394b64c566e7a33bbddb149d9c8
qqbuby <qqbuby@gmail.com>
2017-01-20 14:00:18 +0800

    Add prime.c

---
+++ Numbers/prime.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef enum { true=1, false=0} bool;
+
+bool is_prime(int);
+
+int main(int argc, char *argv[])
+{
+    int n;
+
+    if(argc !=2)
+    {
+        printf("Number required!\n");
+        exit(1);
+    }
+
+    n = atoi(argv[1]);
+
+    while( n > 1)
+    {
+        if(is_prime(n) == true)
+        {
+            printf("%d ", n);
+        }
+        n--;
+    }
+
+    printf("\n");
+
+    exit(0);
+}
+
+bool is_prime(int n)
+{
+    float r;
+    int i; 
+
+    r = sqrtf((float)n);
+
+    for(i=2; i<=r; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+            break;
+        }
+    }
+
+    return true;
+}
