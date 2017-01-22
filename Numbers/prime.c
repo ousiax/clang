5b4527e584acce40ccfda2ee3bf46a416673a9af
CaptainDaVinci <yashkothari199767@gmail.com>
2017-01-22 21:43:07 +0530

    Added a faster implementation of prime number generator

--- Numbers/prime.c
+++ Numbers/prime.c
@@ -1,52 +1,67 @@
+
+/*
+ *  The following is an implementation of
+ *  Sieve of Eratosthenes, which may be considered
+ *  as the fastest prime number generator algorithm
+ *
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
-typedef enum { true = 1, false = 0} bool;
-
-bool is_prime(int);
+void primeGenerator(long int n);
 
 int main(int argc, char *argv[])
 {
-    int n;
-
-    if (argc != 2)
+    if(argc != 2)
     {
-        printf("Number required!\n");
+        printf("usage: %s number\n", argv[0]);
         exit(1);
     }
 
-    n = atoi(argv[1]);
-
-    while ( n > 1)
-    {
-        if (is_prime(n) == true)
-        {
-            printf("%d ", n);
-        }
-        n--;
-    }
-
+    primeGenerator(atoi(argv[1]));
     printf("\n");
-
-    exit(0);
 }
 
-bool is_prime(int n)
+void primeGenerator(long int n)
 {
-    float r;
-    int i;
+    int i, j, count;
 
-    r = sqrtf((float)n);
+    // create a boolean array whose indices represent the numbers.
+    bool *prime = malloc(n + 1);
 
-    for (i = 2; i <= r; i++)
+    // assume all numbers from 2 - n be prime initially.
+    for(i = 2; i <= n; i++)
     {
-        if (n % i == 0)
+        prime[i] = true;
+    }
+
+    // we need to loop from i to sqrt(n), this can be done in a
+    // faster way by looping from i to n with condition i*i <= n
+    // which is faster than calculating square root.
+    for(i = 2; i * i <= n; i++)
+    {
+        count = 0;
+        // in the second loop we eliminate all multiples of i
+        // from being a prime. e.g, if i = 2, we know that 4, 6, 8, 10 ...
+        // will not be prime, hence we change the bool array to false for
+        // all such values upto n.
+        for(j = i * i; j <= n; j = i * i + count * i)
         {
-            return false;
-            break;
+            prime[j] = false;
+            count++;
         }
     }
 
-    return true;
+    for(i = 2; i <= n; i++)
+    {
+        // if prime[i] is true then we know its a prime numbers.
+        if(prime[i])
+        {
+            printf("%d, ", i);
+        }
+    }
 }
